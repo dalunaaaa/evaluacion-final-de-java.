a9f34e47315054592ae184cb499379001a831f07
dalunaaaa <159475285+dalunaaaa@users.noreply.github.com>
2024-10-07 09:44:53 -0600

    Add files via upload

---
+++ src/view/TareasView.java
@@ -0,0 +1,20 @@
+package view;
+
+import model.Tarea;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TareasView  extends JFrame {
+    private int[] pantalla = {1300, 800};
+    private HashMap<String, String> dat = new HashMap<>();
+
+    public TareasView(HashMap<String, String> datos) {
+        this.dat = datos;
+
+
+
+}
+}
